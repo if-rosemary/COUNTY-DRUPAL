27e541aad4280b676764364562d9a468086f02fe
Josh Mitchell <joshuami@users.noreply.github.com>
2021-08-23 16:34:19 -0700

    WCOR-1158: Preliminary configuration files for running DDev environment. (#143)
    Co-authored-by: Lee G. Bridges <17837170+lgbridges@users.noreply.github.com>

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -832,3 +832,9 @@ if (isset($split_filename_prefix)) {
 }
 
 
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
