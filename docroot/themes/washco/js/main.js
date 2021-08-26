3eef8bd69df64b4ca70654b8f840b066b0b75ace
Jung <4228832+gatoona@users.noreply.github.com>
2021-08-26 09:57:33 -0700

    WCOR-1169: Enable Google translate block. (#157)
    * WCOR-1169: Add google translate function.
    
    * Allow for iframing in google translate.
    
    * Additional seckit settings.

--- docroot/themes/washco/js/main.js
+++ docroot/themes/washco/js/main.js
@@ -3,6 +3,15 @@
 
     var body = $("html, body");
 
+    /*Google Translate*/
+    $('.goog-te-combo').change(function() {
+        var lang = $(this).val();
+        if (lang){
+          var url = "https://translate.google.com/translate?hl=en&sl=en&u=" + window.location.href + "&tl=" + lang;
+          window.location.href = url;
+        }
+    });
+
   });
 })(jQuery);
 
