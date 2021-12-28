84d4a7c354ff21b6b2226d61b5d01eaa938989a4
nickm <nick@ifsight.com>
2021-12-28 10:37:57 -0800

    WCOR-22: DRAFT: Adding SCSS, JS and Twig work to support the header navigation component.

--- docroot/themes/custom/westy/src/js/_header.js
+++ docroot/themes/custom/westy/src/js/_header.js
@@ -13,28 +13,48 @@
    * parent menu item. We'd instead like to show them directly below the
    * horizontal stripe of the header.
    *
-   * Since that would take unsetting "position: relative" on many parent
-   * containers, just calculate the container height and set a new "top"
+   * Since relative styling would require un-setting the position of many parent
+   * containers we calculate the container height and set a new "top"
    * position.
    */
-  Drupal.behaviors.westyMenuPosition = {
+  Drupal.behaviors.westyHeaderPosition = {
     attach: function (context) {
-
       const container = document.querySelector(".page__main_header nav.navbar");
       const navLinks = container.querySelectorAll(".nav-item");
 
-      [].forEach.call(navLinks, function(link) {
-
-        link.addEventListener('show.bs.dropdown', function () {
+      [].forEach.call(navLinks, function (link, index) {
+        // Add classes for fitting drop-downs.
+        if (index > 0) {
+          let menu = link.querySelector(".dropdown-menu");
+          if (menu) {
+            menu.classList.add("dropdown-menu-end");
+          }
+        }
+        // Add offset for fitting drop-downs.
+        link.addEventListener("show.bs.dropdown", function () {
           let menu = this.querySelector(".dropdown-menu");
-          menu.style.top =
-            container.offsetHeight - this.offsetHeight + "px";
-        })
-
+          if (menu) {
+            menu.style.top = container.offsetHeight - this.offsetHeight + "px";
+          }
+        });
       });
-
     },
-
   };
 
+  /***
+   * Position navigation search box.
+   */
+  Drupal.behaviors.westyHeaderSearch = {
+    attach: function (context) {
+      const container = document.querySelector(".page__main_header nav.navbar");
+      const searchLink = container.querySelector("[href='/search']");
+      if (searchLink) {
+        const searchBox = container.querySelector("#search-block-form");
+        const menu = searchLink.parentNode.querySelector(".dropdown-menu");
+        if (menu && searchBox) {
+          searchLink.parentNode.replaceChild(searchBox, menu);
+        }
+      }
+    },
+  };
 })(jQuery, Drupal);
