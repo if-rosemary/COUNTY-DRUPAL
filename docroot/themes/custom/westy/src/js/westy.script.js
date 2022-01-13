8a92e487b1d21cbd5b771567e7905644a70e47c0
jlwcor <jung_lee@co.washington.or.us>
2022-01-13 23:57:26 +0000

    WCOR-1249: Alert component development.

--- docroot/themes/custom/westy/src/js/westy.script.js
+++ docroot/themes/custom/westy/src/js/westy.script.js
@@ -18,6 +18,37 @@ import './_header.js';
   //   }
   // };
 
+  // // Controls sitewide alert banner function.
+  Drupal.behaviors.alertSystem = {
+    attach: function (context) {
+      var alertData = {};
+
+      try {
+        alertData = JSON.parse(localStorage.getItem('alertUID')) || {};
+      }
+      catch (e) {
+        console.log(e);
+      }
+
+      $.each(alertData, function (index, value) {
+        $('#' + index).remove();
+      });
+
+      $(".alert-item .btn-close").click(function (e) {
+        var alertUID = $(this).attr('data-alert');
+        alertData[alertUID] = 1;
+        try {
+          localStorage.setItem('alertUID', JSON.stringify(alertData));
+        }
+        catch (e) {
+          return false;
+        }
+      });
+
+      $('.alert-group').removeClass('hidden');
+    }
+  };
+
   // // Enable Bootstrap Toast sitewide.
   // // Toasts are opt-in for performance reasons.
   // Drupal.behaviors.bsToast = {
@@ -25,5 +56,7 @@ import './_header.js';
   //     $('.toast').toast('show');
   //   }
   // };
+
+
 
 })(jQuery, Drupal);
